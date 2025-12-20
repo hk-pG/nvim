29933825062d1a8e44f781dfd93e7bedbedf9d02
hk-pG <hukada.gogotea@gmail.com>
2025-12-20 20:33:24 +0900

    Add escape keymaps for jj, jk, kj in insert mode

---
+++ lua/plugins/keymaps.lua
@@ -0,0 +1,13 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
+      i = {
+        ["jj"] = { "<Esc>", desc = "Escape" },
+        ["jk"] = { "<Esc>", desc = "Escape" },
+        ["kj"] = { "<Esc>", desc = "Escape" },
+      },
+    },
+  },
+}
