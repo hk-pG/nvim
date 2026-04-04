d167a0c20f9ec069a439850ca6c2fca3fc408fbb
hk-pG <hukada.gogotea@gmail.com>
2026-04-05 00:25:33 +0900

    add: implement toggleterm plugin configuration with PowerShell settings for Windows

---
+++ lua/plugins/shell.lua
@@ -0,0 +1,30 @@
+return {
+  "akinsho/toggleterm.nvim",
+  config = function()
+    -- Apply PowerShell settings only on Windows
+    if jit.os == "Windows" then
+      local powershell_options = {
+        shell = "pwsh",
+        shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+        shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+        shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+        shellquote = "",
+        shellxquote = "",
+      }
+
+      for option, value in pairs(powershell_options) do
+        vim.opt[option] = value
+      end
+    end
+    require("toggleterm").setup {
+      size = 10,
+      open_mapping = [[<F7>]],
+      shading_factor = 2,
+      direction = "float",
+      float_opts = {
+        border = "curved",
+        highlights = { border = "Normal", background = "Normal" },
+      },
+    }
+  end,
+}
